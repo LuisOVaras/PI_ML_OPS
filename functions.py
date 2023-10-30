9cfcf6a6f551dc4165bbf1b4079c7264c91c783e
LuisOVaras <vluis2386@gmail.com>
2023-10-30 20:01:28 -0300

    entrega de proyecto

--- functions.py
+++ functions.py
@@ -6,6 +6,8 @@ import pandas as pd  # Asegúrate de importar las bibliotecas necesarias
 df_reviews = pd.read_csv("data/output/reviews.csv")
 df_items = pd.read_parquet("data/output/items.parquet")
 df_steam_games = pd.read_csv("data/output/steam_games.csv")
+
+
 def userdata(user_id: str):
     # Convierte user_id a tipo str
     user_id = str(user_id)
@@ -39,7 +41,7 @@ def userdata(user_id: str):
     }
 
 
-# Optimiza la función UserForGenre
+# Se desarrolla la función UserForGenre
 def UserForGenre(genero: str):
     # Filtra df_steam_games para obtener solo las filas que contienen el género especificado
     filtered_games = df_steam_games[df_steam_games['genres'].str.contains(genero, case=False, na=False)]
@@ -67,3 +69,46 @@ def UserForGenre(genero: str):
     accumulation_list = accumulation.to_dict(orient='records')
 
     return {"Usuario con más horas jugadas para el género " + genero: max_user['user_id'], "Horas jugadas": accumulation_list}
+
+def analyze_developer_data(desarrolladora: str):
+    # Creamos un DataFrame más reducido
+    df_end1 = df_steam_games.loc[:,['developer', 'id', 'price','release_date']]
+    # Utiliza el método str.split para dividir la fecha en sus componentes (Año, Mes, Día) dejando solo el año
+    df_end1['release_date'] = df_end1['release_date'].str.split('-').str.get(0)
+    # Filtra el DataFrame para los juegos de la empresa desarrolladora especificada
+    juegos_desarrolladora = df_end1[df_end1['developer'] == desarrolladora]
+    # Agrupa por año y cuenta la cantidad de juegos
+    juegos_por_ano = juegos_desarrolladora.groupby('release_date').size().reset_index(name='Cantidad de Items')
+    # Filtra los juegos gratuitos
+    juegos_gratuitos = juegos_desarrolladora[juegos_desarrolladora['price'] == 0]
+    # Agrupa los juegos gratuitos por año y cuenta la cantidad
+    juegos_gratuitos_por_ano = juegos_gratuitos.groupby('release_date').size().reset_index(name='cantidad_juegos_gratuitos')
+    # Combina los datos de juegos totales y juegos gratuitos por año
+    resultado = pd.merge(juegos_por_ano, juegos_gratuitos_por_ano, on='release_date', how='left')
+    # Rellena los valores nulos en juegos gratuitos con 0
+    resultado['cantidad_juegos_gratuitos'].fillna(0, inplace=True)
+    # Calcula el porcentaje de juegos gratuitos por año
+    resultado['porcentaje_juegos_gratuitos'] = (resultado['cantidad_juegos_gratuitos'] / resultado['Cantidad de Items']) * 100
+    # Elimina la columna 'cantidad_juegos_gratuitos'
+    resultado.drop(columns='cantidad_juegos_gratuitos', inplace=True)
+    # Renombra las columnas
+    resultado.rename(columns={'release_date': 'Año', 'porcentaje_juegos_gratuitos': 'Contenido Free'}, inplace=True)
+    # Convierte la columna 'Cantidad de Items' a enteros y 'Contenido Free' a flotantes
+    resultado['Cantidad de Items'] = resultado['Cantidad de Items'].astype(int)
+    resultado['Contenido Free'] = resultado['Contenido Free'].astype(float)
+    # Convierte el DataFrame a una lista de diccionarios para asegurarse de que los tipos de datos sean estándar de Python (evita errores en FastAPI)
+    resultado_dict_list = resultado.to_dict(orient='records')
+    return resultado_dict_list
+
+def analyze_developer_reviews(desarrolladora: str):
+    # Filtrar las reseñas por el desarrollador dado
+    reseñas_desarrolladora = df_reviews[df_reviews['user_id'].isin(df_items[df_items['item_id'].isin(df_steam_games[df_steam_games['developer'] == desarrolladora]['id'])]['user_id'])]
+
+    # Contar las reseñas con sentimiento positivo y negativo
+    sentimiento_positivo = reseñas_desarrolladora[reseñas_desarrolladora['sentiment_analysis'] == 2].shape[0]
+    sentimiento_negativo = reseñas_desarrolladora[reseñas_desarrolladora['sentiment_analysis'] == 0].shape[0]
+
+    # Crear el diccionario de resultados
+    resultado = {desarrolladora: {'Positive': sentimiento_positivo, 'Negative': sentimiento_negativo}}
+
+    return resultado
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 from recomendacion_juego import recomendacion_juego
 
 # Importar las funciones desde functions.py
-from functions import userdata, UserForGenre
+from functions import userdata, UserForGenre, analyze_developer_data, analyze_developer_reviews
 
 # Cargar los datos CSV
 df_reviews = pd.read_csv("data/output/reviews.csv")
@@ -24,31 +24,27 @@ async def get_recomendacion_juego(id_producto: int):
 
     return {"recomendaciones": result}
 
-# 2
+@app.get('/developer_data/{desarrolladora}')
+def developer(desarrolladora: str):
+    resultado = analyze_developer_data( desarrolladora)
+    return resultado
+
+
 @app.get("/user_data/{user_id}")
 async def get_user_data(user_id: str):
     result = userdata(user_id)
     return result
 
-# 3
+
 @app.get("/user_genre/{genero}")
 async def get_user_genre(genero: str):
     result = UserForGenre(genero)
     return result
 
-# 5 Aquí desarrollamos el ultimo endpoint:
-@app.get("/developer/{desarrolladora}")
+
+@app.get('/developer_reviews/{desarrolladora}')
 def developer(desarrolladora: str):
-    # Filtrar las reseñas por el desarrollador dado
-    reseñas_desarrolladora = df_reviews[df_reviews['user_id'].isin(df_items[df_items['item_id'].isin(df_steam_games[df_steam_games['developer'] == desarrolladora]['id'])]['user_id'])]
-
-    # Contar las reseñas con sentimiento positivo y negativo
-    sentimiento_positivo = reseñas_desarrolladora[reseñas_desarrolladora['sentiment_analysis'] == 2].shape[0]
-    sentimiento_negativo = reseñas_desarrolladora[reseñas_desarrolladora['sentiment_analysis'] == 0].shape[0]
-
-    # Crear el diccionario de resultados
-    resultado = {desarrolladora: {'Positive': sentimiento_positivo, 'Negative': sentimiento_negativo}}
-
+    resultado = analyze_developer_reviews( desarrolladora)
     return resultado
 
 #Run server
